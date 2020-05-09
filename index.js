0ffa8f563a2e9c6cdc60b4989c2a7b4b93d0c5f3
fraricce <fraricce@gmail.com>
2020-05-09 02:22:06 +0200

    directions

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
-'use strict';
-var inquirer = require('inquirer');
-console.log('Hello welcome to your adventure');
-var questions = [
-  {
-    type: 'input',
-    name: 'action',
-    message: "What's your next action?"
-  }
-];
+"use strict";
+var inquirer = require("inquirer");
+console.log("Hello welcome to your adventure");
+var directionsPrompt = {
+  type: 'list',
+  name: 'direction',
+  message: 'Which direction would you like to go?',
+  choices: ['Forward', 'Right', 'Left', 'Back', 'Quit']
+};
 
-function ask() {
-  let currentScene = questions;
-  while (true) {
-    inquirer.prompt(questions).then(answers => {
-      console.log('You said: ' + answers.action)
-      if (answers.action !== 'quit') {
-        ask();
-      } else {
-        console.log('Your action:', answers.action);
-      }
-    });
-  }
-  
+function main(index) {
+  console.log('You find youself in a small room, there is a door in front of you.');
+  runScene(index);
 }
 
-ask();
+let world = [{
+  type: 'list',
+  name: 'direction',
+  message: 'Which direction would you like to go?',
+  choices: ['Right', 'Quit']
+}, {
+  type: 'list',
+  name: 'direction2',
+  message: 'Which direction would you like to look go?',
+  choices: ['Left', 'Quit']
+}];
+
+var index = 0;
+
+function getQuestions(x) {
+  return world[x];
+}
+
+function runScene(x) {
+  let questions = getQuestions(x);
+  inquirer.prompt(questions).then(answers => {
+    if (answers.direction === 'Right') {
+      console.log('Go right');
+      runScene(++index);
+    }
+
+    if (answers.direction2 === 'Left') {
+      console.log('Go left');
+      runScene(--index);
+    }
+    
+    if (answers.direction === 'Quit') {
+      console.log('Bye');
+      return;
+    }
+  });
+}
+
+main(index);
